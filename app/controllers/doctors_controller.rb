29029bbe195d4b54fe679cf7ac080f14d2b4ca3b
Derick Castillo <derick.castillo@flatironschool.com>
2020-11-23 14:49:52 -0600

    Configured show page functionality

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,4 +2,14 @@ class DoctorsController < ApplicationController
   def index
     @doctors = Doctor.all
   end
+
+  def show
+    @doctor = Doctor.find(params[:id])
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :age)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # resources :appointments
   # resources :patients
-  # resources :doctors
+  resources :doctors, only: [:index, :show]
 
-  get "/doctors", to: "doctors#index"
+  # get "/doctors", to: "doctors#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
