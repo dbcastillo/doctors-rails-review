59682d676a7e0acbc26bcd272471f6347d0c5403
Derick Castillo <derick.castillo@flatironschool.com>
2020-11-23 13:54:28 -0600

    set up model relations and seeds

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,10 @@
 class Doctor < ApplicationRecord
+  has_many :appointments
+  has_many :patients, through: :appointments
+
+  def self.most_popular_doctor
+    self.all.max_by do |doctor|
+      doctor.appointments.size
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Appointment.destroy_all
+Doctor.destroy_all
+Patient.destroy_all
+
+derick = Doctor.create(name: "Derick", age: 55)
+max = Doctor.create(name: "Max", age: 99)
+emily = Doctor.create(name: "Emily", age: 3)
+
+henry = Patient.create(name: "Henry", age: 9)
+kathy = Patient.create(name: "Kathy", age: 23)
+rancid = Patient.create(name: "Rancid", age: 87)
+
+a1 = Appointment.create(doctor_id: derick.id, patient_id: henry.id)
+a2 = Appointment.create(doctor_id: max.id, patient_id: kathy.id)
+a3 = Appointment.create(doctor_id: derick.id, patient_id: rancid.id)
