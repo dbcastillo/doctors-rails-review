3b33970aa7480829e0098b81d748ba698cb022ea
Derick Castillo <derick.castillo@flatironschool.com>
2020-11-23 14:41:18 -0600

    Setup logic to see all doctors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :appointments
-  resources :patients
-  resources :doctors
+  # resources :appointments
+  # resources :patients
+  # resources :doctors
+
+  get "/doctors", to: "doctors#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
